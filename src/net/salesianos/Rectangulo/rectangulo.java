9b10cb60572239ef5746d135429a03c8fc857c8f
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-05-20 17:49:57 +0100

    Añadimos lo metodos de interfaz para calcular el area y el perimetro del rectangulo y a suz vez usando Override para implementar la interfaz

--- src/net/salesianos/Rectangulo/rectangulo.java
+++ src/net/salesianos/Rectangulo/rectangulo.java
@@ -10,4 +10,14 @@ public class rectangulo implements interfazFiguras  {
         this.altura = altura;
     }
 
+    @Override
+    public double calcularArea() {
+        return base * altura;
+    }
+
+    @Override 
+    public double calcularPerimetro() {
+        return 2 * (base + altura);
+    }
+    
 }
