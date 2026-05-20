f63b2e35a14591fd435922da224259393ec6536a
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-05-20 19:59:14 +0100

    Ponemos un mensaje para que el usuario introduzca la vase y la altura, y a su vez el objeto para almacenar la informacion

--- App.java
+++ App.java
@@ -7,5 +7,17 @@ public class App {
     public static void main(String[] args){
 
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Introduce la base del rectangulo:");
+        double base = scanner.nextDouble();
+
+        System.out.println("Introduce la altura del rectangulo:");
+        double altura = scanner.nextDouble();
+
+        rectangulo rectangulo1 = new rectangulo(base, altura);
+
+
+
+
     }
 }
