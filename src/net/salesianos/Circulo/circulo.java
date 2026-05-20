557ac7f0249587e4bae6395ea04b41b1cec6c310
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-05-20 18:27:38 +0100

    Pongo un atributo para almacenar el radio del circulo y el constructor para iniciarlo, tambien la palabra clave implements

--- src/net/salesianos/Circulo/circulo.java
+++ src/net/salesianos/Circulo/circulo.java
@@ -1,5 +1,12 @@
 package src.net.salesianos.Circulo;
+import src.net.salesianos.InterfazFiguras.interfazFiguras;
 
-public class circulo {
+public class circulo implements interfazFiguras {
     
+    private double radio;
+
+    public circulo(double radio) {
+        this.radio = radio;
+    }
 }
+
