6e3ae120b72ab71fe22cada7d65582598951b505
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-05-20 18:45:20 +0100

    He añadido un par de comentarios

--- src/net/salesianos/Circulo/circulo.java
+++ src/net/salesianos/Circulo/circulo.java
@@ -3,27 +3,30 @@ import src.net.salesianos.InterfazFiguras.interfazFiguras;
 
 public class circulo implements interfazFiguras {
     
+    //Añado el atributo radio para el circulo
     private double radio;
 
     public circulo(double radio) {
         this.radio = radio;
     }
 
-    //Añado 
+    //Añado los metodos para calcular el area y el perimetro del circulo
     @Override
     public double calcularArea() {
         return Math.PI * Math.pow(radio, 2);
     }
 
+    //Ponemos MathPI para calcular el perimetro del circulo
     @Override
     public double calcularPerimetro() {
         return 2 * Math.PI * radio;
     }
 
+    //y el metodo toString para mostrar la informacion del circulo
     @Override
     public String toString() {
         return "Circulo => Radio: " + radio;
     }
-    
+
 }
 
