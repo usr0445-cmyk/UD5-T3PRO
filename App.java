12f6e61ad84c52690e303d80e7dbe45d3d881dfd
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-05-20 19:29:19 +0100

    He puesto los imports necesarios para la clase ejecutable, y he preparado el Scanner para pedir  y poner la informacion al usuario

--- App.java
+++ App.java
@@ -1,3 +1,11 @@
+import java.util.Scanner;
+import src.net.salesianos.Rectangulo.rectangulo;
+import src.net.salesianos.Circulo.circulo;
+
+
 public class App {
-    
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+    }
 }
