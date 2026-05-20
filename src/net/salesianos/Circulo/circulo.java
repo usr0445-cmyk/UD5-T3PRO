53772ace0abeccdcd25de7d1405fea72cc339047
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-05-20 18:33:54 +0100

    Metodos para calcular el area y perimetro, poniedolo con sus calculos correspondientes

--- src/net/salesianos/Circulo/circulo.java
+++ src/net/salesianos/Circulo/circulo.java
@@ -8,5 +8,16 @@ public class circulo implements interfazFiguras {
     public circulo(double radio) {
         this.radio = radio;
     }
+
+    //Añado 
+    @Override
+    public double calcularArea() {
+        return Math.PI * Math.pow(radio, 2);
+    }
+
+    @Override
+    public double calcularPerimetro() {
+        return 2 * Math.PI * radio;
+    }
 }
 
