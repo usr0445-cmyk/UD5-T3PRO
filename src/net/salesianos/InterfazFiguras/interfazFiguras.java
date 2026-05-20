e6bad9a96d553f128c9edcc95f5b6cbe2d9dadd5
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-05-20 17:17:36 +0100

    Añadido un comentario en la clase interfazFiguraes

--- src/net/salesianos/InterfazFiguras/interfazFiguras.java
+++ src/net/salesianos/InterfazFiguras/interfazFiguras.java
@@ -2,6 +2,7 @@ package src.net.salesianos.InterfazFiguras;
 
 public class interfazFiguras {
     
+    //Interfaz con los metodos de calculo geométrico 
     public interface CalculoBiodimensional {
         double calcularArea();
         double calcularPerimetro();
