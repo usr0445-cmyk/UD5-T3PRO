9ffb16475f52203233141f7a49beb5f89c7b7b22
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-05-21 16:27:24 +0100

    Añado un par de comentarios y cerramos el scanner al acabar la clase ejecutable

--- App.java
+++ App.java
@@ -6,6 +6,7 @@ import src.net.salesianos.Circulo.circulo;
 public class App {
     public static void main(String[] args){
 
+        //Usamos el Scanner para pedir al usuario los datos
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Introduce la base del rectangulo:");
@@ -14,13 +15,16 @@ public class App {
         System.out.println("Introduce la altura del rectangulo:");
         double altura = scanner.nextDouble();
 
+        //Creamos un objeto rectangulo con los datos introducidos por el usuario
         rectangulo rectangulo1 = new rectangulo(base, altura);
 
+        //Mostramos la informacion del rectangulo y el area y perimetro calculados
         System.out.println(rectangulo1.toString());
         System.out.println("Area del rectangulo: " + rectangulo1.calcularArea());
         System.out.println("Perimetro del rectangulo: " + rectangulo1.calcularPerimetro());
 
-        
+
+        //Lo mismo para el circulo, lo unico que cambia es el radio 
         System.out.println("Introduce el radio del circulo: ");
         double radio = scanner.nextDouble();
 
@@ -30,10 +34,6 @@ public class App {
         System.out.println("Area del circulo: " + circulo1.calcularArea());
         System.out.println("Perimetro del circulo: " + circulo1.calcularPerimetro());
 
-
-        
-
-
-
+        scanner.close();
     }
 }
