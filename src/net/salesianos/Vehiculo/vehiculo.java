11809283511a75c93aada81ce2f084817eb99529
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-05-21 17:14:36 +0100

    Hacemos la clase abstracta y ponemos atributos con protected para que puedan ser accedidos por las clases hijas

--- src/net/salesianos/Vehiculo/vehiculo.java
+++ src/net/salesianos/Vehiculo/vehiculo.java
@@ -1,5 +1,12 @@
 package src.net.salesianos.Vehiculo;
 
-public class vehiculo {
+public abstract class vehiculo {
     
+    protected String color;
+    protected int numeroPuertas;
+    protected int numeroRuedas;
+    protected String modelo;
+    protected String matricula;
+
+
 }
