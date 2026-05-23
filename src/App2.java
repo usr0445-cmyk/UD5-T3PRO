6f8c089f5c8ef9f1f792a466444695f4ec38cbeb
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-05-23 15:39:24 +0100

    Comprobamos el combustible restante despues de mover el coche varias veces, y luego cambiamos el combustible a 2 litros para probar el comportamiento del coche con poco combustible

--- src/App2.java
+++ src/App2.java
@@ -1,5 +1,6 @@
 package src;
-import src.net.salesianos.CocheCombustion.cocheCombustion;;
+import src.net.salesianos.CocheCombustion.cocheCombustion;
+import src.net.salesianos.Vehiculo.vehiculo;
 
 public class App2 {
     public static void main(String[] args) {
@@ -17,9 +18,15 @@ public class App2 {
         coche1.mover();
         coche1.mover();
 
+        System.out.println("Combustible restante: " + coche1.litrosCombustible);
 
+        System.out.println("Cambiando combustible a 2 litros...");
+        coche1.litrosCombustible = 2;
 
-
+        System.out.println("Moviendo el coche 3 veces con 2 litros...");
+        coche1.mover();
+        coche1.mover();
+        coche1.mover();
 
 
 
