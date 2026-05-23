1ec10f6f413771634984f7be33f101dc54f7ae49
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-05-23 15:27:19 +0100

    Estamos en la clase ejecutable, aqui  creamos un objeto de la clase cocheCombustion para probar su funcionamiento y despues mostramos informacion

--- src/App2.java
+++ src/App2.java
@@ -3,6 +3,11 @@ import src.net.salesianos.CocheCombustion.cocheCombustion;;
 
 public class App2 {
     public static void main(String[] args) {
+
+        cocheCombustion coche1 = new cocheCombustion("Rojo", 4, 4, "Seat Ibiza", "1234 ABC", 20);
+
+        //y aqui 
+        System.out.println("Intentando arrancar el coche...");
         
 
 
@@ -12,6 +17,6 @@ public class App2 {
 
 
 
-        
+
     }   
 }
