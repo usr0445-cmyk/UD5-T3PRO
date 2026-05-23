3233a53d7195570204061f8345dc9e6aeb2e9c41
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-05-23 10:54:41 +0100

    He añadido un par de comentarios a la clase vehiculo

--- src/net/salesianos/Vehiculo/vehiculo.java
+++ src/net/salesianos/Vehiculo/vehiculo.java
@@ -2,12 +2,14 @@ package src.net.salesianos.Vehiculo;
 
 public abstract class vehiculo {
     
+    //Añado los atributos comunes a todos los vehiculos
     protected String color;
     protected int numeroPuertas;
     protected int numeroRuedas;
     protected String modelo;
     protected String matricula;
 
+    //Constructor para inicializar los atributos del vehiculo
     public vehiculo(String color, int numeroPuertas, int numeroRuedas, String modelo, String matricula) {
         this.color = color;
         this.numeroPuertas = numeroPuertas;
@@ -16,10 +18,13 @@ public abstract class vehiculo {
         this.matricula = matricula;
     }
 
+    //Aqui añado los metodos abstractos para que las clases hijas los implementen
     public abstract void arrancar();
     public abstract void pararMotor();
     public abstract void mover();
 
+
+    //Añado los Getters necesarios
     public String getColor() {
         return color;
     }
