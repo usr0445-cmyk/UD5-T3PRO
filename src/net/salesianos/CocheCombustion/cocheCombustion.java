ddd1ebea85d02b1b8fec4d44e0ab8e5ca6aba75a
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-05-23 15:13:44 +0100

    He añadido un par de comentarios

--- src/net/salesianos/CocheCombustion/cocheCombustion.java
+++ src/net/salesianos/CocheCombustion/cocheCombustion.java
@@ -1,11 +1,14 @@
 package src.net.salesianos.CocheCombustion;
 import src.net.salesianos.Vehiculo.vehiculo;
 
+//Aplicamos el extends y el import de la clase Vehiculo
 public class cocheCombustion extends vehiculo {
     
+    //Ponemos atributos para el coche de combustion, como el estado del motor y los litros de combustible
     protected boolean motorEncendido;
     protected int litrosCombustible;
 
+    //Contructor para inicializar los atributos del coche de combustion, incluyendo los atributos heredados de la clase vehiculo
     public cocheCombustion(String color, int numeroPuertas, int numeroRuedas, String modelo, String matricula, int litrosCombustible) {
         super(color, numeroPuertas, numeroRuedas, modelo, matricula);
 
@@ -13,10 +16,11 @@ public class cocheCombustion extends vehiculo {
         this.motorEncendido = false;
     }
 
-
+    //Aqui ponemos los metodos para arrancar, parar el motor y mover el coche implementando los metodos abstractos de la clase vehiculo
     @Override
     public void arrancar(){
         if (litrosCombustible > 0){
+            //Esto para restar un litro de combustible cada vez que se arranca el coche
             litrosCombustible--;
             motorEncendido = true;
             System.out.println("El coche ha arrancado. Combustible restante: " + litrosCombustible);
@@ -26,6 +30,7 @@ public class cocheCombustion extends vehiculo {
         }
     }
 
+    //Igual aqui pero para parar el motor, comprobando si el motor esta encendido o no  
     @Override
     public void pararMotor() {
         if (motorEncendido) {
@@ -36,6 +41,7 @@ public class cocheCombustion extends vehiculo {
         }
     }
 
+    //Y aqui el metodo para mover el coche, comprobando si el motor esta encendido y si hay combustible suficiente para moverse
     @Override
     public void mover() {
         if (!motorEncendido) {
