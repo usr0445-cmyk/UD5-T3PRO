ea8e3bcb01315801bec3089551b6abbc52a7bc98
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-05-23 14:21:41 +0100

    Añado atributos protected para el motor y el combustible del coche de combustion, y depues pongo el constructor para inicializar los atributos del coche de combustion, llamando al constructor de la clase padre vehiculo con super

--- src/net/salesianos/CocheCombustion/cocheCombustion.java
+++ src/net/salesianos/CocheCombustion/cocheCombustion.java
@@ -1,5 +1,17 @@
 package src.net.salesianos.CocheCombustion;
+import src.net.salesianos.Vehiculo.vehiculo;
 
-public class cocheCombustion {
+public class cocheCombustion extends vehiculo {
     
+    protected boolean motorEncendido;
+    protected int litrosCombustible;
+
+    public cocheCombustion(String color, int numeroPuertas, int numeroRuedas, String modelo, String matricula, int litrosCombustible) {
+        super(color, numeroPuertas, numeroRuedas, modelo, matricula);
+
+        this.litrosCombustible = litrosCombustible;
+        this.motorEncendido = false;
+    }
+
+
 }
