6b3b531ee4ac7411d9b378df04615b9bd1aec556
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-05-23 15:33:34 +0100

    Aqui probamos a arrancar el coche, moverlo varias veces y luego intentar arrancarlo de nuevo para ver que no se puede arrancar porque se ha quedado sin combustible

--- src/App2.java
+++ src/App2.java
@@ -6,11 +6,16 @@ public class App2 {
 
         cocheCombustion coche1 = new cocheCombustion("Rojo", 4, 4, "Seat Ibiza", "1234 ABC", 20);
 
-        //y aqui 
         System.out.println("Intentando arrancar el coche...");
-        
+        coche1.arrancar();
 
+        System.out.println("Arrancando...");
+        coche1.arrancar();
 
+        System.out.println("Moviendo el coche varias veces...");
+        coche1.mover();
+        coche1.mover();
+        coche1.mover();
 
 
 
