9e960528978168dc80c733248c83eb761a3a8f71
usr0445-cmyk <usr0445@salesianos-lacuesta.net>
2026-05-23 15:18:56 +0100

    Preaparamos la App para desarrollar el codigo

--- src/App2.java
+++ src/App2.java
@@ -1,4 +1,17 @@
 package src;
+import src.net.salesianos.CocheCombustion.cocheCombustion;;
+
 public class App2 {
-    
+    public static void main(String[] args) {
+        
+
+
+
+
+
+
+
+
+        
+    }   
 }
